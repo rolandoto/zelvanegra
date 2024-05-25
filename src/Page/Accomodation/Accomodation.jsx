5668d1c3c5fdd3db61901fcf8e750edbaf5b9232
Rolando Guerrero <rolando22_@outlook.com>
2024-05-25 01:19:16 -0700

    Update Accomodation.jsx

--- src/Page/Accomodation/Accomodation.jsx
+++ src/Page/Accomodation/Accomodation.jsx
@@ -18,13 +18,16 @@ import ContentLoader from "react-content-loader";
 
 
 const LoadingSkeleton =(props) =>{
-  return (      <ContentLoader height={1000} width={700} >
+  return (  <ContentLoader
+      height={1000}
+      width={700}
+      {...props}
+    >
       <circle cx="25" cy="50" r="25" />
       <circle cx="25" cy="150" r="25" />
       <circle cx="25" cy="250" r="25" />
       <circle cx="25" cy="330" r="25" />
-    
-     
+
       <rect x="60" y="30" rx="5" ry="5" width="220" height="15" />
       <rect x="60" y="50" rx="5" ry="5" width="70" height="15" />
       <rect x="140" y="50" rx="5" ry="5" width="90" height="15" />
@@ -80,10 +83,7 @@ const LoadingSkeleton =(props) =>{
       <rect x="320" y="350" rx="5" ry="5" width="100" height="15" />
       <rect x="380" y="350" rx="5" ry="5" width="50" height="15" />
       <rect x="440" y="350" rx="5" ry="5" width="60" height="15" />
-
-    
-     
-    </ContentLoader>)
+    </ContentLoader>   )
 }
 
 
