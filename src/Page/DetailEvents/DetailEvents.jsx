2ecc7b5e4c8827730ca14e53ab56dc72522d7df4
Jhonathan Velez <kokoa.arte@gmail.com>
2024-07-26 18:26:50 -0500

    initial commit update

--- src/Page/DetailEvents/DetailEvents.jsx
+++ src/Page/DetailEvents/DetailEvents.jsx
@@ -67,7 +67,7 @@ const DetailEvents =() =>{
     }
     return (<>
                <Header/>
-               <div className="relative bg-cover bg-center h-[410px]" style={{ backgroundImage: `url(https://github.com/rolandoto/image-pms/blob/main/_MG_4223.jpg?raw=true)`,}}>
+               <div className="relative bg-cover bg-center h-[410px]" style={{ backgroundImage: `url(https://grupo-hoteles.com/storage/app/23/page/1746688908-23-page-slider-1-_MG_4609-HDR.jpg)`,}}>
                     <div className="absolute inset-0 bg-black opacity-50"></div>
                     <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
                         <h1 className="text-4xl md:text-6xl lg:text-6xl font-lora">
